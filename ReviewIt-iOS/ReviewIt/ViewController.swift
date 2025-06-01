d883bd04c16524853cef4f0ab07303840f200d83
정채은 <cce523@hanmail.net>
2025-06-01 17:15:33 +0900

    Setting - Create Project

---
+++ ReviewIt-iOS/ReviewIt/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReviewIt
+//
+//  Created by 정채은 on 5/14/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
