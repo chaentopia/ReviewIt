18face6b38be007ab052b7d4180196e96413c47d
정채은 <cce523@hanmail.net>
2025-06-01 19:31:03 +0900

    Feat [#7] ui 구현 및 티켓 추가 버튼 로직 연결
    collectionview 제외

---
+++ ReviewIt-iOS/ReviewIt/Presentation/Ticket/AddTicketViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AddTicketViewController.swift
+//  ReviewIt
+//
+//  Created by 정채은 on 6/1/25.
+//
+
+import UIKit
+
+class AddTicketViewController: BaseViewController {
+    
+}
--- ReviewIt-iOS/ReviewIt/Presentation/Ticket/TicketViewController.swift
+++ ReviewIt-iOS/ReviewIt/Presentation/Ticket/TicketViewController.swift
@@ -7,23 +7,64 @@
 
 import UIKit
 
-class TicketViewController: UIViewController {
+import SnapKit
+import Then
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class TicketViewController: BaseViewController {
+    
+    private let titleView  = TitleView(title: StringLiterals.TicketMain.title,
+                                       isLeftButtonHidden: true,
+                                       isRightButtonHidden: true)
+    lazy var ticketCollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: ticketFlowLayout
+    )
+    private let ticketFlowLayout = UICollectionViewFlowLayout()
+    lazy var addTicketButton = UIButton()
+    
+    override func setStyle() {
+        super.setStyle()
+        addTicketButton.do {
+            $0.setImage(.btnAddTicket, for: .normal)
+        }
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func setLayout() {
+        let safeAreaHeight = view.safeAreaInsets.bottom
+        let tabBarHeight = tabBarController?.tabBar.frame.height ?? 75.adjustedHeight
+        
+        self.view.addSubviews(titleView,
+                              ticketCollectionView,
+                              addTicketButton)
+        
+        titleView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            $0.centerX.height.equalToSuperview()
+            $0.height.equalTo(48.adjustedHeight)
+        }
+        
+        ticketCollectionView.snp.makeConstraints {
+            $0.top.equalTo(titleView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        addTicketButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(16)
+            $0.bottom.equalTo(safeAreaHeight).offset(-tabBarHeight - 18.adjustedHeight)
+            $0.size.equalTo(60)
+        }
     }
-    */
-
+    
+    override func setAddTarget() {
+        addTicketButton.addTarget(self,
+                                  action: #selector(pushAddTicketViewController),
+                                  for: .touchUpInside)
+    }
+    
+    @objc private func pushAddTicketViewController() {
+        let addTicketViewController = AddTicketViewController()
+        addTicketViewController.hidesBottomBarWhenPushed = true
+        self.navigationController?.pushViewController(addTicketViewController, animated: true)
+    }
 }
