beb2e3f91142ada995db01b23fae657e9349b58a
정채은 <cce523@hanmail.net>
2025-06-02 15:47:39 +0900

    Feat [#18] Kakao 로그인 추가

--- ReviewIt-iOS/ReviewIt/Application/SceneDelegate.swift
+++ ReviewIt-iOS/ReviewIt/Application/SceneDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import KakaoSDKAuth
+
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
@@ -22,7 +24,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         self.window = window
         self.window?.makeKeyAndVisible()
     }
-
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        if let url = URLContexts.first?.url {
+            if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                _ = AuthController.handleOpenUrl(url: url)
+            }
+        }
+    }
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
--- ReviewIt-iOS/ReviewIt/Presentation/Splash/SplashViewController.swift
+++ ReviewIt-iOS/ReviewIt/Presentation/Splash/SplashViewController.swift
@@ -7,40 +7,106 @@
 
 import UIKit
 
+import KakaoSDKAuth
+import KakaoSDKUser
 import SnapKit
 import Then
 
 final class SplashViewController: BaseViewController {
     
     let logoImageView = UIImageView()
+    let kakaoLoginButton = UIButton()
+    
+    var isLogin = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigateToNextScreen()
     }
     
+    override func setAddTarget() {
+        kakaoLoginButton.addTarget(self, action: #selector(kakaoLoginButtonTapped), for: .touchUpInside)
+    }
+    
     override func setStyle() {
         view.backgroundColor = .mainBurgundy
         logoImageView.image = .reviewItLogo
+        
+        kakaoLoginButton.do {
+            $0.setImage(.btnKakaoLogin, for: .normal)
+            $0.alpha = 0
+        }
     }
     
     override func setLayout() {
-        self.view.addSubviews(logoImageView)
+        self.view.addSubviews(logoImageView,
+                              kakaoLoginButton)
         
         logoImageView.snp.makeConstraints {
             $0.center.equalToSuperview()
+        }
+        
+        kakaoLoginButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.bottom.equalToSuperview().inset(109)
+            $0.height.equalTo(44)
         }
     }
     
     private func navigateToNextScreen() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
             var nextViewController: UIViewController
-        
-            nextViewController = MainTabBarViewController()
             
-            let navigationController = UINavigationController(rootViewController: nextViewController)
-            UIApplication.shared.windows.first?.rootViewController = navigationController
-            UIApplication.shared.windows.first?.makeKeyAndVisible()
-        }   
+            if self.isLogin {
+                nextViewController = MainTabBarViewController()
+                
+                let navigationController = UINavigationController(rootViewController: nextViewController)
+                UIApplication.shared.windows.first?.rootViewController = navigationController
+                UIApplication.shared.windows.first?.makeKeyAndVisible()
+            } else {
+                self.kakaoLoginButton.alpha = 1
+            }
+        }
+    }
+    
+    @objc private func kakaoLoginButtonTapped() {
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            kakaoLoginWithApp()
+        } else {
+            kakaoLoginWithAccount()
+        }
+    }
+    
+    func kakaoLoginWithApp() {
+        UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("loginWithKakaoTalk() success.")
+                let nextViewController = MainTabBarViewController()
+                let navigationController = UINavigationController(rootViewController: nextViewController)
+                UIApplication.shared.windows.first?.rootViewController = navigationController
+                UIApplication.shared.windows.first?.makeKeyAndVisible()
+                _ = oauthToken
+            }
+        }
+    }
+    
+    func kakaoLoginWithAccount() {
+        UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+            if let error = error {
+                print(error)
+            }
+            else {
+                print("loginWithKakaoAccount() success.")
+                let nextViewController = MainTabBarViewController()
+                let navigationController = UINavigationController(rootViewController: nextViewController)
+                UIApplication.shared.windows.first?.rootViewController = navigationController
+                UIApplication.shared.windows.first?.makeKeyAndVisible()
+                
+                _ = oauthToken
+            }
+        }
     }
 }
