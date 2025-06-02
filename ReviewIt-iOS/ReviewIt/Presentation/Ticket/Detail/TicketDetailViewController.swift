8f4c855fa733d0e1daabc6aadc761761e7b752f4
정채은 <cce523@hanmail.net>
2025-06-02 14:36:09 +0900

    Feat [#13] 태그 리뷰 하단 부분 추가
    높이 조정 필요, 태그 리뷰 작성 여부에 따른 레이아웃 변화 필요

--- ReviewIt-iOS/ReviewIt/Presentation/Ticket/Detail/TicketDetailViewController.swift
+++ ReviewIt-iOS/ReviewIt/Presentation/Ticket/Detail/TicketDetailViewController.swift
@@ -16,17 +16,12 @@ final class TicketDetailViewController: BaseViewController {
                                     title: "마타하리",
                                     date: Date.now,
                                     cast: ["노윤", "옥주현"],
-                                    place: "LG이트센터서울 LG SIGNATURE홀",
+                                    place: "LG아트센터서울 LG SIGNATURE홀",
                                     seat: ["1", "A", "3", "20"],
                                     platform: "멜론티켓",
                                     price: 130000,
                                     reviewStatus: true,
-                                    type1: [0,1,2],
-                                    type2: [0,4,5],
-                                    type3: ["노윤", "옥주현"],
-                                    type4: [1],
-                                    type5: [1,2],
-                                    type6: ["인생 공연", "재관람 의사 있음", "레전드", "레전드", "한 번만 보기 좋음", "무난함"])
+                                    typeList: [[0,1,2], [0,4,5], [1, 2], [1], [1,2],[2,3,4,5]])
     
     private let titleView = TitleView(title: StringLiterals.TicketDetail.title,
                                        isLeftButtonHidden: false,
@@ -36,9 +31,9 @@ final class TicketDetailViewController: BaseViewController {
     let titleLabel = UILabel()
     let posterImageView = UIImageView()
     let ticketTitleLabel = TicketDetailLabel()
-    let dateTitleLabel = TicketDetailLabel()
-    let castTitleLabel = TicketDetailLabel()
-    let placeTitleLabel = TicketDetailLabel()
+    let dateLabel = TicketDetailLabel()
+    let castLabel = TicketDetailLabel()
+    let placeLabel = TicketDetailLabel()
     
     let seatLabel = UILabel()
     let seatStackView = UIStackView()
@@ -47,8 +42,19 @@ final class TicketDetailViewController: BaseViewController {
     let seatRowLabel = TicketSeatDetailLabel()
     let seatNumLabel = TicketSeatDetailLabel()
     
-    let platformTitleLabel = TicketDetailLabel()
-    let priceTitleLabel = TicketDetailLabel()
+    let platformLabel = TicketDetailLabel()
+    let priceLabel = TicketDetailLabel()
+    
+    lazy var reviewTagCollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: reviewTagFlowLayout
+    )
+    private let reviewTagFlowLayout = UICollectionViewFlowLayout()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setCollectionView()
+    }
     
     override func setStyle() {
         self.view.backgroundColor = .mainWhite
@@ -74,15 +80,15 @@ final class TicketDetailViewController: BaseViewController {
             $0.text = ticketData.title
         }
         
-        dateTitleLabel.do {
+        dateLabel.do {
             $0.text = DateFormatter.ticketDisplayFormatter.string(from: ticketData.date)
         }
         
-        castTitleLabel.do {
+        castLabel.do {
             $0.text = ticketData.cast.joined(separator: ", ")
         }
         
-        placeTitleLabel.do {
+        placeLabel.do {
             $0.text = ticketData.place
         }
         
@@ -114,11 +120,11 @@ final class TicketDetailViewController: BaseViewController {
             $0.text = "\(ticketData.seat[3])번"
         }
         
-        platformTitleLabel.do {
+        platformLabel.do {
             $0.text = ticketData.platform
         }
         
-        priceTitleLabel.do {
+        priceLabel.do {
             let formatter = NumberFormatter()
             formatter.numberStyle = .decimal
             formatter.locale = Locale(identifier: "ko_KR")
@@ -127,6 +133,18 @@ final class TicketDetailViewController: BaseViewController {
                 let result = "₩\(formattedPrice)"
                 $0.text = result
             }
+        }
+        
+        reviewTagCollectionView.do {
+            $0.showsHorizontalScrollIndicator = false
+            $0.showsVerticalScrollIndicator = false
+            $0.backgroundColor = .mainWhite
+        }
+        
+        reviewTagFlowLayout.do {
+            $0.minimumLineSpacing = 20
+            $0.scrollDirection = .vertical
+            $0.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
         }
     }
     
@@ -137,13 +155,14 @@ final class TicketDetailViewController: BaseViewController {
         scrollView.addSubviews(titleLabel,
                                posterImageView,
                                ticketTitleLabel,
-                               dateTitleLabel,
-                               castTitleLabel,
-                               placeTitleLabel,
+                               dateLabel,
+                               castLabel,
+                               placeLabel,
                                seatLabel,
                                seatStackView,
-                               platformTitleLabel,
-                               priceTitleLabel)
+                               platformLabel,
+                               priceLabel,
+                               reviewTagCollectionView)
         
         seatStackView.addArrangedSubviews(seatFloorLabel,
                                           seatSectionLabel,
@@ -181,26 +200,26 @@ final class TicketDetailViewController: BaseViewController {
             $0.height.equalTo(44)
         }
         
-        dateTitleLabel.snp.makeConstraints {
+        dateLabel.snp.makeConstraints {
             $0.top.equalTo(ticketTitleLabel.snp.bottom).offset(16)
             $0.leading.trailing.width.equalTo(ticketTitleLabel)
             $0.height.equalTo(ticketTitleLabel)
         }
         
-        castTitleLabel.snp.makeConstraints {
-            $0.top.equalTo(dateTitleLabel.snp.bottom).offset(16)
+        castLabel.snp.makeConstraints {
+            $0.top.equalTo(dateLabel.snp.bottom).offset(16)
             $0.leading.trailing.width.equalTo(ticketTitleLabel)
             $0.height.equalTo(ticketTitleLabel)
         }
         
-        placeTitleLabel.snp.makeConstraints {
-            $0.top.equalTo(castTitleLabel.snp.bottom).offset(16)
+        placeLabel.snp.makeConstraints {
+            $0.top.equalTo(castLabel.snp.bottom).offset(16)
             $0.leading.trailing.width.equalTo(ticketTitleLabel)
             $0.height.equalTo(ticketTitleLabel)
         }
         
         seatLabel.snp.makeConstraints {
-            $0.top.equalTo(placeTitleLabel.snp.bottom).offset(16)
+            $0.top.equalTo(placeLabel.snp.bottom).offset(16)
             $0.leading.equalTo(ticketTitleLabel)
         }
         
@@ -209,18 +228,31 @@ final class TicketDetailViewController: BaseViewController {
             $0.leading.equalTo(ticketTitleLabel)
         }
         
-        platformTitleLabel.snp.makeConstraints {
+        platformLabel.snp.makeConstraints {
             $0.top.equalTo(seatStackView.snp.bottom).offset(16)
             $0.leading.trailing.width.equalTo(ticketTitleLabel)
             $0.height.equalTo(ticketTitleLabel)
         }
         
-        priceTitleLabel.snp.makeConstraints {
-            $0.top.equalTo(platformTitleLabel.snp.bottom).offset(16)
+        priceLabel.snp.makeConstraints {
+            $0.top.equalTo(platformLabel.snp.bottom).offset(16)
             $0.leading.trailing.width.equalTo(ticketTitleLabel)
             $0.height.equalTo(ticketTitleLabel)
-            $0.bottom.equalToSuperview().inset(21)
         }
+        
+        reviewTagCollectionView.snp.makeConstraints {
+            $0.top.equalTo(priceLabel.snp.bottom).offset(45)
+            $0.width.equalTo(SizeLiterals.Screen.screenWidth - 48)
+            $0.leading.trailing.equalToSuperview().inset(24)
+            $0.height.equalTo(SizeLiterals.Screen.screenHeight)
+            $0.bottom.equalToSuperview().inset(45)
+        }
+    }
+    
+    private func setCollectionView() {
+        reviewTagCollectionView.delegate = self
+        reviewTagCollectionView.dataSource = self
+        reviewTagCollectionView.register(TagGroupDetailCollectionViewCell.self, forCellWithReuseIdentifier: TagGroupDetailCollectionViewCell.className)
     }
     
     override func setAddTarget() {
@@ -236,5 +268,23 @@ final class TicketDetailViewController: BaseViewController {
         // 정보 넘기는 로직 추가
         let addTicketViewController = AddTicketViewController()
         self.navigationController?.pushViewController(addTicketViewController, animated: true)
+    }
+}
+
+extension TicketDetailViewController: UICollectionViewDelegate { }
+
+extension TicketDetailViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let nonEmptyTypeCount = ticketData.typeList.filter { !$0.isEmpty }.count
+        return nonEmptyTypeCount
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: TagGroupDetailCollectionViewCell.className,
+            for: indexPath) as? TagGroupDetailCollectionViewCell else { return UICollectionViewCell() }
+        cell.configCell(data: indexPath.item,
+                        tagList: ticketData.typeList[indexPath.item])
+        return cell
     }
 }
